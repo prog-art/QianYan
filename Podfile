e3a1b50be08e9a2e62d15d3a0efbe9d7aa38b11b
RuiZhang <tigercat.ruizhang@gmail.com>
2015-04-28 00:10:18 +0800

    add ASyncSocket by CocoaPods

--- Podfile
+++ Podfile
@@ -3,6 +3,8 @@
 
 target 'QianYan_Ios' do
 
+pod 'CocoaAsyncSocket', '~> 7.4.1'
+
 end
 
 target 'QianYan_IosTests' do
---
+++ Pods/Target Support Files/Pods-QianYan_Ios/Pods-QianYan_Ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QianYan_Ios : NSObject
+@end
+@implementation PodsDummy_Pods_QianYan_Ios
+@end
